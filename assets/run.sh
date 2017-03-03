e74bdfebe544884d1df832f9a0d10f336d1bc9d4
Nico <xkodiak@talarionstudios.com>
2017-03-03 02:02:19 +0100

    short url and change command

--- assets/run.sh
+++ assets/run.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-uuid=$(curl -s "http://rancher-metadata/2015-12-19/self/container/uuid/")
-args="--bind all"
+url='http://rancher-metadata/2015-12-19'
+uuid=$(curl -s "$url/self/container/uuid/")
+cmd="rethinkdb --bind all"
 
 while read -r line; do
     id=$(echo $line | grep -oP '[0-9](?=\=)')
     hostname=$(echo $line | grep -oP '(?<=\=).+')
 
-    if [ $(curl -s "http://rancher-metadata/2015-12-19/self/service/containers/$id/uuid/") != "$uuid" ]; then
-        args="$args --join $hostname"
+    if [ $(curl -s "$url/self/service/containers/$id/uuid/") != "$uuid" ]; then
+        cmd="$cmd --join $hostname"
     fi
-done < <(curl -s 'http://rancher-metadata/2015-12-19/self/service/containers/')
+done < <(curl -s "$url/self/service/containers/")
 
-exec "rethinkdb" "$args"
-
+echo "$cmd"
+echo "----------------------"
+$cmd
